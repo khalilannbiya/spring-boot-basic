7f6b63768374f2d7682ebaa2465e1a78974e8c59
khalilannbiya <syeichkhalil@gmail.com>
2022-05-07 19:24:20 +0700

    Business Layer

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+  public List<Student> getStudents() {
+    return List.of(
+        new Student(
+            1L,
+            "Mariam",
+            "mariam.jamal@gmail.com",
+            LocalDate.of(2000, Month.JANUARY, 5),
+            22),
+        new Student(
+            2L,
+            "Khalil",
+            "syeichkhalil@gmail.com",
+            LocalDate.of(2001, Month.JUNE, 30),
+            21));
+  }
+}
