88846c55f1735b9845592357f5d5565db0aae273
khalilannbiya <syeichkhalil@gmail.com>
2022-05-08 19:06:28 +0700

    update student

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,5 +38,25 @@ public class StudentService {
       throw new IllegalStateException("student with id" + studentId + " does not exists");
     }
     studentRepository.deleteById(studentId);
+  }
+
+  @Transactional
+  public void updateStudent(Long studentId,
+      String name,
+      String email) {
+    Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException(
+        "student with id " + studentId + " does not exists"));
+
+    if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+
+    if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+      Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+      if (studentOptional.isPresent()) {
+        throw new IllegalStateException("email taken");
+      }
+      student.setEmail(email);
+    }
   }
 }
